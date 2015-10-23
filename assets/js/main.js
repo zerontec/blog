73226e1552a8b01c61cc78209dece4852a3d705f
Leo <zerontecnologia1@gmail.com>
2015-10-23 17:37:36 -0430

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,109 @@
+jQuery(function($) {
+
+	//#main-slider
+	$(function(){
+		$('#main-slider.carousel').carousel({
+			interval: 8000
+		});
+	});
+
+	$( '.centered' ).each(function( e ) {
+		$(this).css('margin-top',  ($('#main-slider').height() - $(this).height())/2);
+	});
+
+	$(window).resize(function(){
+		$( '.centered' ).each(function( e ) {
+			$(this).css('margin-top',  ($('#main-slider').height() - $(this).height())/2);
+		});
+	});
+
+
+
+	//portfolio
+	$(window).load(function(){
+		$portfolio_selectors = $('.portfolio-filter >li>a');
+		if($portfolio_selectors!='undefined'){
+			$portfolio = $('.portfolio-items');
+			$portfolio.isotope({
+				itemSelector : 'li',
+				layoutMode : 'fitRows'
+			});
+			$portfolio_selectors.on('click', function(){
+				$portfolio_selectors.removeClass('active');
+				$(this).addClass('active');
+				var selector = $(this).attr('data-filter');
+				$portfolio.isotope({ filter: selector });
+				return false;
+			});
+		}
+	});
+
+	//contact form
+	var form = $('.contact-form');
+	form.submit(function () {
+		$this = $(this);
+		$.post($(this).attr('action'), function(data) {
+			$this.prev().text(data.message).fadeIn().delay(3000).fadeOut();
+		},'json');
+		return false;
+	});
+
+	//goto top
+	$('.gototop').click(function(event) {
+		event.preventDefault();
+		$('html, body').animate({
+			scrollTop: $("body").offset().top
+		}, 500);
+	});	
+
+	//Pretty Photo
+	$("a[rel^='prettyPhoto']").prettyPhoto({
+		social_tools: false
+	});	
+
+	$ (function() {
+
+	//cache de windows object
+
+	var $window = $(window);
+
+	//parallax backgroud effect
+	$('section [data-type="backgroud"]').each(function(){
+
+		var $bgobj = $ (this); //assigning the object
+
+		$(windows).scroll(function(){
+
+			//scroll the backgroud at var speed
+			// the ypost is a negative value  because we are scrolling it up 
+
+
+			var yPos = -($windows.scrollTop() /
+				$bgobj.data('speed'));
+
+			//put together our final backgroud position
+
+			var coords= '50%' + yPos + 'px';
+
+			//move the backgroud 
+
+			$bgobj .css({backgroudPosition: coords});
+
+
+
+		});
+
+	});
+
+});
+
+
+
+
+
+});
+
+
+
+
+
